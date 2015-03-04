dcc215c56443d4f8c10b367c51b6b8483ae1dbba
Danny Tizon <dtizon@gmail.com>
2015-03-04 11:18:58 -0800

    setup log4j2 logging to console

--- src/main/java/com/gapperdan/hswm/controller/HelloController.java
+++ src/main/java/com/gapperdan/hswm/controller/HelloController.java
@@ -1,18 +1,26 @@
 package com.gapperdan.hswm.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
 @EnableWebMvc
 public class HelloController {
 
+    static final Logger logger = LogManager.getLogger(HelloController.class.getName());
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String hello() {
+        logger.info("inside HelloController");
         return "world!";
     }
 
